bd72d10c1cc2b400cafe4cb743d91f83750db545
dakshpandey177@gmail.com <dakshpandey177@gmail.com>
2025-05-01 13:41:28 +0530

    bias memory module

---
+++ neuron/bias/bias_memory.sv
@@ -0,0 +1,39 @@
+`include "./../definitions.sv"
+
+module bias_memory #(
+    parameter data_bits = 16,
+    parameter bias_file = "pretrained_bias.mif"
+)(
+    input logic clk,
+    input logic reset,
+    input logic write_en,
+    input logic read_en,
+    input logic [data_bits-1:0] bias_in,
+    output logic [data_bits-1:0] bias_out
+);
+
+    // Only one bias value stored
+    reg [data_bits-1:0] bias_memory;
+
+    // Pretrained mode: load bias from file
+    `ifdef pretrained
+        initial begin
+            $readmemb(bias_file, memory); // Assumes file has just 1 line
+        end
+    `else
+        always @(posedge clk) begin
+            if (reset) begin
+                memory <= '0;
+            end else if (write_en) begin
+                memory <= bias_in;
+            end
+        end
+    `endif
+
+    always @(posedge clk) begin
+        if (read_en) begin
+            bias_out <= memory;
+        end
+    end
+
+endmodule
